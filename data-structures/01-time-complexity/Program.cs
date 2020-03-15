8b2d82f678fa12cb8a418a5fe5920cfa4c8b0a85
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-03-15 18:30:22 +0530

    time complexity for triplet sum integer

---
+++ data-structures/01-time-complexity/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _01_time_complexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var triplets = TripletSum.Count(new int[] { 1, 3, -2, 1, -3, 0, 2 });
+
+            watch.Stop();
+
+            Console.WriteLine($"The number of zerp-sum triplets :{triplets}");
+            Console.Write($"The time taken: {watch.Elapsed:g}");
+            Console.Read();
+        }
+    }
+}
---
+++ data-structures/01-time-complexity/TripletSum.cs
@@ -0,0 +1,38 @@
+﻿namespace _01_time_complexity
+{
+    /// <summary>
+    ///  //1,3,-2,1,-3,0,2
+    //1,3,-2
+    //1,3,1
+    //1,3,-3
+    //1,3,0
+    //1,3,2
+    //Next triplet by increment j and reset k
+    //1,-2,1
+    //1,-2,1 ....
+    /// </summary>
+    public class TripletSum
+    {
+        public static int Count(int[] a)
+        {
+            int n = a.Length;
+            int counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    for (int k = j + 1; k < n; k++)
+                    {
+                        if(a[i] + a[j] + a[k] == 0)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            return counter;
+        }
+    }
+}
