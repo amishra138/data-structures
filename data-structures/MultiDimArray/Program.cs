b39930ac9054c60b0b9be611dd18fde9a77eb566
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-03-15 19:15:56 +0530

    added jagged array in c#

---
+++ data-structures/MultiDimArray/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace MultiDimArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JaggedArraysDemo();
+
+            Console.ReadLine();
+        }
+
+        private static void MultiDimArrays()
+        {
+            int[,] r1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] r2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < r2.GetLength(0); i++)
+            {
+                for (int j = 0; j < r2.GetLength(1); j++)
+                {
+                    Console.WriteLine($"{r2[i, j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void JaggedArraysDemo()
+        {
+            int[][] jaggedArray = new int[4][];
+
+            jaggedArray[0] = new int[1];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+            jaggedArray[3] = new int[4];
+
+            Console.WriteLine("Enter the numbers for a jagged array");
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    string st = Console.ReadLine();
+
+                    jaggedArray[i][j] = int.Parse(st);
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Printing the elements");
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j]);
+                    Console.Write("\0");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
