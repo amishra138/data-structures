85b2a2f2adcadf7c7e20a3a61bafb8165977dc6b
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-05-14 14:37:05 +0530

    tested program to count linked list length and find pair of sum in array

--- data-structures/ProgrammingTest/Program.cs
+++ data-structures/ProgrammingTest/Program.cs
@@ -8,9 +8,29 @@ namespace ProgrammingTest
         {
             Console.WriteLine("Welcome to programming test!");
 
-            string result = StringGenerator.GenerateString(5, 3);
+            #region String Generator
+            //string result = StringGenerator.GenerateString(5, 3);
 
-            Console.WriteLine("Result is - " + result);
+            //Console.WriteLine("Result is - " + result);
+            #endregion
+
+            #region Find Pair in Array whose sum is provided as input
+            //Console.WriteLine(FindPairInArray.Find(new int[] { 1, 5, 6, 8, 10, 15 }, 20));
+            #endregion
+
+            #region Linked List: Length Count
+            LinkedList llist = new LinkedList();
+            llist.Push(1);
+            llist.Push(3);
+            llist.Push(1);
+            llist.Push(2);
+            llist.Push(1);
+
+            Console.WriteLine($"Linked list count by iterative method: {llist.GetCountByInterative()}");
+
+            Console.WriteLine($"Linked list count by iterative method: {llist.GetCountByRecursive()}");
+
+            #endregion
 
             Console.ReadLine();
         }
