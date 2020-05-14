1ead1e789af8eda0812543858095777a0ee3e88d
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-05-14 14:36:09 +0530

    Find if pair exists in an array whose sum = K

---
+++ data-structures/ProgrammingTest/FindPairInArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ProgrammingTest
+{
+    public class FindPairInArray
+    {
+        /// <summary>
+        /// Find if pair exists in an array whose sum = K
+        /// </summary>
+        /// <param name="arr">Array</param>
+        /// <param name="k">Sum of pair</param>
+        /// <returns>True if matches</returns>
+        public static bool Find(int[] arr, int k)
+        {
+            bool isFound = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == k)
+                    {
+                        isFound = true;
+                        break; //added break to improve complexity
+                    }
+                }
+
+                if (isFound) //added break to improve complexity
+                    break;
+            }
+
+            return isFound;
+        }
+    }
+}
